16d3c27a1b4327149cca6f2793278c3cb95e9933
Alibek Muhumov <RemakeRr@users.noreply.github.com>
2017-07-08 22:44:16 +0600

    -

--- source/pages/index/index.js
+++ source/pages/index/index.js
@@ -1,4 +1,11 @@
 import './index.sass';
 import 'normalize.css';
 import 'animate.css';
+import 'bootstrap-datepicker';
 
+$('.input-group.date').datepicker({
+    clearBtn: true,
+    language: "ru",
+    autoclose: true,
+    todayHighlight: true
+});
